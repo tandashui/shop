c924c4ac1459cdd37dc078f6f1dca0b0200d4201
tandashui <tandashui8686@163.com>
2016-11-13 15:00:49 +0800

    cehi

---
+++ Application/Runtime/Data/_fields/tpshop.tp_wx_keyword.php
@@ -0,0 +1,1 @@
+a:7:{i:0;s:2:"id";i:1;s:7:"keyword";i:2;s:3:"pid";i:3;s:5:"token";i:4;s:4:"type";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:7:"int(11)";s:7:"keyword";s:9:"char(255)";s:3:"pid";s:7:"int(11)";s:5:"token";s:11:"varchar(60)";s:4:"type";s:11:"varchar(30)";}}
